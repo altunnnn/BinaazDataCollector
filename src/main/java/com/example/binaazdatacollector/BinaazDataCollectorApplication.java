4f4afe830df927a068fbba3e5f6feed8f9f9ec3e
user <tarverdiyevaltun@gmail.com>
2023-02-03 20:25:20 +0400

    Initial commit

---
+++ src/main/java/com/example/binaazdatacollector/BinaazDataCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.example.binaazdatacollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaazDataCollectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinaazDataCollectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/binaazdatacollector/BinaazDataCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.binaazdatacollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinaazDataCollectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
